d312eb9dd3ae599ac2f6ce666f5f96417ebf0183
Akash Mourya <mouryakash883@gmail.com>
2021-07-18 01:40:11 +0530

    Final changes

--- middleware/index.js
+++ middleware/index.js
@@ -1,54 +1,54 @@
-var Campground= require("../models/campgrounds");
-var Comment= require("../models/comments");
+var art = require('../models/arts');
+var Comment = require('../models/comments');
 
 var middlewareObj = {};
 
-middlewareObj.checkCampgroundOwnership = function(req,res,next){
-    if(req.isAuthenticated()){
-        Campground.findById(req.params.id, function(err, foundCampground){
-            if(err || !foundCampground){
-                req.flash("error", "Campground not found")
-                res.redirect("back")
-            }else{
-                if(foundCampground.author.id.equals(req.user._id)){
-                    next();
-                }else{
-                    req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-                }
-            }
-        });
-    }else{
-        req.flash("error","You need to be logged in to do that");
-        res.redirect("back");
-    }
-}
-middlewareObj.checkCommentOwnership = function(req,res,next){
-    if(req.isAuthenticated()){
-        Comment.findById(req.params.comment_id, function(err, foundComment){
-            if(err || !foundComment){
-                req.flash("error", "Comment not found")
-                res.redirect("back")
-            }else{
-                if(foundComment.author.id.equals(req.user._id)){
-                    next();
-                }else{
-                    req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-                }
-            }
-        });
-    }else{
-        req.flash("error","You need to be logged in to do that");
-        res.redirect("back");
-    }
-}
+middlewareObj.checkartOwnership = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    art.findById(req.params.id, function (err, foundart) {
+      if (err || !foundart) {
+        req.flash('error', 'art not found');
+        res.redirect('back');
+      } else {
+        if (foundart.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash('error', "You don't have permission to do that");
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    req.flash('error', 'You need to be logged in to do that');
+    res.redirect('back');
+  }
+};
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err || !foundComment) {
+        req.flash('error', 'Comment not found');
+        res.redirect('back');
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash('error', "You don't have permission to do that");
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    req.flash('error', 'You need to be logged in to do that');
+    res.redirect('back');
+  }
+};
 
-middlewareObj.isLoggedIn = function(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error","You need to be logged in to do that")
-    res.redirect("/login");
-}
+middlewareObj.isLoggedIn = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash('error', 'You need to be logged in to do that');
+  res.redirect('/login');
+};
 module.exports = middlewareObj;
